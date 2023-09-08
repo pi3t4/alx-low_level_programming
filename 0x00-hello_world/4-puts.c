ddda046f4a0931b3117460c76a24e6c2c55533c1
pi3t4 <tablun45@gmail.com>
2023-09-08 15:15:48 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
-int  main(void)
-{
-
-  /**
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-  puts("/"Programming is like building a multilingual puzzle")
-       return (0);
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        
+  return (0);
 }
+
+
+
+
